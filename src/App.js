59f15665a9ec25e5ca8751ca23e92febf65c240a
Rafael <rafaelgarrafiel@gmail.com>
2018-08-21 21:25:51 -0500

    Fix SearchPage - App

--- src/App.js
+++ src/App.js
@@ -6,58 +6,70 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    books: [],
-    showningBooks: []
-  }
-  componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
-  }
+    state = {
+        books: [],
+        showingBooks: []
+    }
+    // componentDidMount() {
+    //     BooksAPI.getAll().then((books) => {
+    //         this.setState({ books })
+    //     })
+    // }
+    async componentDidMount() {
+        const books = await BooksAPI.getAll()
+        this.setState({ books })
+    }
 
 
-  updateBook = (book, shelf) => {
-    BooksAPI.update(book, shelf).then((data) => {
+    updateBook = (book, shelf) => {
+        BooksAPI.update(book, shelf).then((data) => {
+            const updatedBook = {
+                ...book,
+                shelf
+            }
+            this.setState(state => ({
+                books: state.books.filter(b => b.id !== updatedBook.id).concat([updatedBook])
+            }))
+        })
+    }
 
-      const updatedBook = {
-        ...book,
-        shelf
-      }
-      this.setState(state => ({
-        books: state.books.filter(b => b.id !== updatedBook.id).concat([updatedBook])
-      }))
-    })
-  }
+    
 
-  searchBook = (query) => {
-    BooksAPI.search(query).then((showningBooks) => {
-      if (showningBooks.error) {
-        this.setState({ showningBooks: []})
-      } else {
-        this.setState({ showningBooks})
-      }
-    })
-  }
+    searchBook = (query) => {
+        if (query) {
+            BooksAPI.search(query).then((showingBooks) => {
 
-  render() {
-    return (
-      <div className="app">
-        <Route path="/" exact render={() => (
-          <ListPage 
-            updateBook={this.updateBook}
-            books={this.state.books}
-          />
-        )} />
-        <Route path="/search" render={({history})=>(
-          <SearchPage 
-            showningBooks={this.state.showningBooks}
-            updateBook={this.updateBook}
-            searchBook={this.searchBook}/>
-        )} />
-      </div>
-    )
-  }
+                if (showingBooks && showingBooks.error) {
+                    this.setState({ showingBooks: []})
+                } else {
+                    this.setState(state => ({
+                        showingBooks: showingBooks
+                    }))
+                }
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="app">
+            <Route path="/" exact render={() => (
+                <ListPage 
+                updateBook={this.updateBook}
+                books={this.state.books}
+                />
+                )} />
+            <Route path="/search" render={({history})=>(
+                <SearchPage 
+                books={this.state.books}
+                showingBooks={this.state.showingBooks}
+                updateBook={this.updateBook}
+                searchBook={this.searchBook}
+                />
+                )} />
+            </div>
+            )
+    }
 }
 
 export default BooksApp
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {Link} from 'react-router-dom'
+import { Debounce } from 'react-throttle';
 import Book from "./Book";
 
 class SearchPage extends Component {
@@ -10,10 +11,16 @@ class SearchPage extends Component {
     updateQuery = (query) => {
         this.setState({ query: query.trim() })
         this.props.searchBook(query)
-    } 
+    }
+
+    verifyBook = (searchedBook) => {
+        const findedBook = this.props.books.find(book=>book.id===searchedBook.id)
+        findedBook ? searchedBook.shelf = findedBook.shelf : searchedBook.shelf = 'none'
+        return searchedBook
+    }
 
     render(){
-        const { updateBook, showningBooks } = this.props
+        const { updateBook, showingBooks } = this.props
         const { query } = this.state
 
         return(
@@ -22,25 +29,30 @@ class SearchPage extends Component {
                     <div className="search-books-bar">
                         <Link to="/" className="close-search">Close</Link>
                         <div className="search-books-input-wrapper">
-                            <input 
-                                type="text" 
-                                placeholder="Search by title or author" 
-                                onChange={(event) => this.updateQuery(event.target.value)}/>
+                            <Debounce time="400" handler="onChange">
+                                <input 
+                                    type="text" 
+                                    placeholder="Search by title or author" 
+                                    onChange={(event) => this.updateQuery(event.target.value)}/>
+                            </Debounce>
                         </div>
                     </div>
                     <div className="search-books-results">
                         {query && (
                             <ol className="books-grid">
-                                {showningBooks.map((book) => (
-                                    <li key={book.id}>
-                                        <Book book={book} updateBook={updateBook}></Book>
-                                    </li>
-                                ))}
+                                {showingBooks.map( book => {
+                                    this.verifyBook(book)
+                                    return(
+                                        <li key={book.id}>
+                                            <Book book={book} updateBook={updateBook}></Book>
+                                        </li>
+                                    )
+                                })}
                             </ol>
                         )}
-                        {showningBooks.length === 0 && query && (
+                        {showingBooks.length === 0 && query && (
                             <div className="search-books-results">
-                                <span>Exibindo {showningBooks.length} Livros</span>
+                                <span>Exibindo {showingBooks.length} Livros</span>
                             </div>
                         )}
                     </div>
